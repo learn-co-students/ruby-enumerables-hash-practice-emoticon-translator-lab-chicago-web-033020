3fa5a487c36a7e2ecbb864580d33b8cfb10a5f71
Shau Yin Luk <ivan.luk@icloud.com>
2020-03-27 19:14:50 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,7 @@ end
 def get_japanese_emoticon(file, engemo)
   # code goes here
   hash1 = load_library(file)
-  ej_hash = hash1[:get_emoticon]
-  jemo = ej_hash[engemo]
+  jemo = hash1[:get_emoticon][engemo]
   if jemo == nil 
     return msg = "Sorry, that emoticon was not found"
   else
@@ -30,8 +29,7 @@ end
 def get_english_meaning(file, jemo_in)
   # code goes here
   hash2 = load_library(file)
-  m_hash = hash2[:get_meaning]
-  mean_out = m_hash[jemo_in]
+  mean_out = hash2[:get_meaning][jemo_in]
   if mean_out == nil 
     return msg = "Sorry, that emoticon was not found"
   else
