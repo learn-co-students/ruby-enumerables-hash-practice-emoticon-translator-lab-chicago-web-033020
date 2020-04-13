092e2bed846c3a98569af8f59592a03684a732b8
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 09:09:33 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,5 @@
-# require modules here
+# Other ways: https://github.com/deniznida/emoticon-translator-ruby-007-public/blob/master/lib/translator.rb
+require 'pry'
 require "yaml"
 
 def load_library(file_path)
@@ -7,21 +8,38 @@ def load_library(file_path)
   library.each do |meaning, emoticons|
     #the keys inside the 'get_meaning' hash are the Japanese emoticons
     #the emoticon keys inside the 'get_meaning' hash point to their meanings
-  result["get_meaning"][emoticons[1]] = meaning
+  #{"get_meaning"=>{"☜(⌒▽⌒)☞"=>"angel"}
+                       #emoticons[1]=> "☜(⌒▽⌒)☞"
+  result["get_meaning"][emoticons[1]] = meaning  # "angel"
   #the keys inside the 'get_emoticon' hash are the English emoticons
   #the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents
-  result["get_emoticon"][emoticons[0]] = emoticons[1]
+  #"get_emoticon"=>{"O:)"=>"☜(⌒▽⌒)☞"}
+  #emoticons[0]=> "O:)"                 #emoticons[1]=> "☜(⌒▽⌒)☞"
+  result["get_emoticon"][emoticons[0]] = emoticons[1]  #"☜(⌒▽⌒)☞"
   end
   result
+  # {"get_meaning"=>{"☜(⌒▽⌒)☞"=>"angel"}, "get_emoticon"=>{"O:)"=>"☜(⌒▽⌒)☞"}}
 end
 
+
+# This method will take a traditional Western emoticon, like :) and translate it to its Japanese version.
+# It will rely load_library to work. Refer to the table below for translations
 def get_japanese_emoticon(file_path, emoticon)
-  japanese_emoticon = load_library(file_path)['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : "Sorry, that emoticon was not found"
-
+  japanese_emoticon = load_library(file_path)["get_emoticon"][emoticon] # "(＾ｖ＾)"
+   japanese_emoticon ? japanese_emoticon : "Sorry, that emoticon was not found"
+  # if japanese_emoticon == nil
+  #   japanese_emoticon = "Sorry, that emoticon was not found"
+  # end
+  # japanese_emoticon
 end
 
+# It will take a Japanese emoticon and returns its meaning in English.
+# This method will also rely on load_library to first load the YAML file.
 def get_english_meaning(file_path, emoticon)
-  meaning = load_library(file_path)['get_meaning'][emoticon]
+  meaning = load_library(file_path)["get_meaning"][emoticon]  #sad
   meaning ? meaning : "Sorry, that emoticon was not found"
+  # if meaning == nil
+  #   meaning = "Sorry, that emoticon was not found"
+  # end
+  # meaning
 end
